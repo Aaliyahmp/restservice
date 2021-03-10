46ad900cd148665bfe9389e630f0f3829e17350b
Aaliyah <74381305+Aaliyahmp@users.noreply.github.com>
2021-03-10 14:49:50 -0600

    Add files via upload

---
+++ gs-rest-service/initial/src/main/java/com/example/restservice/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.restservice.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public @ResponseBody String greeting() {
+        return "Hello, World";
+    }
+}
